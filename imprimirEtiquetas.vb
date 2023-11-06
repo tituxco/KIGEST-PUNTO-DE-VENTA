d419141ad2396b8d00d15883f539a8eaa4db8ded
Roberto Quatrin <roberto.quatrin@gmail.com>
2023-11-06 19:35:36 -0300

    se redujo el tamaño de la etiqueta #1 a 50x25mm

--- imprimirEtiquetas.vb
+++ imprimirEtiquetas.vb
@@ -688,8 +688,7 @@ Public Class imprimirEtiquetas
                 pgSize.Height = 173.23 '100
             ElseIf My.Settings.TipoEtiqueta = 1 Then
                 pgSize.Width = 196.8 '  135 '180 '196.8 '
-                pgSize.Height = 118  '78 '173.23 '100
-
+                pgSize.Height = 98.4  '78 '173.23 '100
             End If
 
             PrintTxt.DefaultPageSettings.PaperSize = pgSize
